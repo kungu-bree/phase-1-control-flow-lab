1b3d3b2d6a7c84c1780cdb7424289193563514d3
Brenda <brendakungu04@gmail.com>
2024-02-11 23:44:55 +0300

    JS conditionals if ternary switch

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2000 && feet <2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    return 'No can do.';
+  }
+  
+}
+
+function ternaryCheckCity(city) {
+   return (city === 'NYC' ) ? 'Ok, sounds good.':'No go.';
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
+  }
   // Write your code here!
 }
